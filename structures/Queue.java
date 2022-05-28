6c325159b320be3597d4a371c1495f1c86a4d3eb
cuhs <alexhu00374@gmail.com>
2022-05-28 09:55:12 -0400

    Update Queue.java

--- structures/Queue.java
+++ structures/Queue.java
@@ -3,18 +3,12 @@ package structures;
 import java.util.NoSuchElementException;
 
 /**
- * A generic queue implementation.
- * 
- * @author RU-NB-CS 112
- *
- * @param <T> Parameter type for items in the stack.
+ * queue implementation.
  */
 public class Queue<T> {
 	
 	/**
 	 * Node used for queue's linked list
-	 * 
-	 * @param <E> Parameter type for items in the queue
 	 */
 	static class Node<E> {
 		E data; 
@@ -25,14 +19,7 @@ public class Queue<T> {
 		}
 	}
 	
-	/**
-	 * Rear of queue
-	 */
 	private Node<T> rear;
-	
-	/**
-	 * Number of items in the queue
-	 */
 	private int size;
 	
 	public Queue() {
@@ -42,40 +29,35 @@ public class Queue<T> {
 	
 	/**
 	 * Adds an item to the end of the queue
-	 * 
-	 * @param item Item to be enqueued
 	 */
 	public void enqueue(T item) {
-		Node<T> ptr = new Node<T>(item, null);
+		Node<T> p = new Node<T>(item, null);
 		if (rear == null) {
-			ptr.next = ptr;
+			p.next = p;
 		} else {
-			ptr.next = rear.next;
-			rear.next = ptr;
+			p.next = rear.next;
+			rear.next = p;
 		}
-		size++;
-		rear = ptr;
+		++size;
+		rear = p;
 	}
 	
 	/**
 	 * Deletes and returns the front of the queue
 	 * 
-	 * @return Item at the front of the queue
-	 * @throws NoSuchElementException If the queue is empty
 	 */
-	public T dequeue() 
-	throws NoSuchElementException {
+	public T dequeue() {
 		if (rear == null) {
-			throw new NoSuchElementException("queue is empty");
+			return null;
 		}
-		T hold = rear.next.data;
+		T temp = rear.next.data;
 		if (size == 1) {
 			rear = null;
 		} else {
 			rear.next = rear.next.next;
 		}
 		size--;
-		return hold;
+		return temp;
 	}
 	
 	/**
@@ -84,13 +66,12 @@ public class Queue<T> {
 	 * @return True if queue is empty, false if not.
 	 */
 	public boolean isEmpty() {
-		return size == 0;
+		if(size==0) return true;
+		return false;
 	}
 	
 	/**
 	 * Returns number of items in queue.
-	 * 
-	 * @return Number of items in queue.
 	 */
 	public int size() {
 		return size;
